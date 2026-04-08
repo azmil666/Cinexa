f7b383cf5cff95cb1e68416cff37421424064e90
Azmil Mohammed <azmilmohammed369@gmail.com>
2026-04-08 17:49:38 +0530

    fix: add alt attributes

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Result = ({ result, openPopup }) => {
   return (
     <div className="result" onClick={() => openPopup(result.imdbID)}>
-        <img src={result.Poster} />
+        <img src={result.Poster} alt={result.Title}/>
         <h3>{result.Title}</h3>
     </div>
   )
